34fb30e722961da9ac4fb2aa143de414c75ae6b5
asammyo <aakilasammy@gmail.com>
2022-12-12 19:55:56 -0400

    Web Project

---
+++ Popup.js
@@ -0,0 +1,7 @@
+function showNewsletter() {
+    let email = prompt("Enter your email address if you'd like to sign up")
+
+    if (email) {
+        alert(`great, we will email you at ${email}`);
+    }
+}
